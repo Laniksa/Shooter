28f5553d190e1fcba118c853398db2cf7587a47a
Регина <rmashkovtseva@gmail.com>
2023-02-07 15:15:01 +0300

    Программа закончена

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,17 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        Player player = new Player("Вася");
+
+        System.out.format("У игрока " + player.getName() + ", " + player.getSlotsCount() + " слотов с оружием,"
+                        + " введите номер ( от 0 до " + player.getSlotsCount() + " ), чтобы выстрелить,"
+                        + " -1 чтобы выйти%n");
+        int slot = scan.nextInt();
+        player.shotWithWeapon(slot);
     }
 }
--- src/com/company/Player.java
+++ src/com/company/Player.java
@@ -3,21 +3,49 @@ package com.company;
 import Weapon.Weapon;
 import Weapon.BigGun;
 import Weapon.WaterPistol;
+import Weapon.Pistol;
+import Weapon.RPG;
+import Weapon.Slingshot;
 
 public class Player {
     private String name;
     private Weapon[] weaponSlots ;
-    private int sizeWeapon = 10;
+    private int sizeWeapon = 5;
 
-    public Player(String name, Weapon[] weaponSlots){
+    public Player(String name){
         this.name = name;
         weaponSlots = new Weapon[sizeWeapon];
         weaponSlots[0] = new BigGun();
         weaponSlots[1] = new WaterPistol();
+        weaponSlots[2] = new Pistol();
+        weaponSlots[3] = new RPG();
+        weaponSlots[4] = new Slingshot();
+
+
     }
 
     public int getSlotsCount() {//количество слотов
         return weaponSlots.length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void shotWithWeapon(int slot) {
+        if(slot==-1){
+            System.out.println("Выход из программы");
+            return;
+        }else if (slot < 0 || slot > weaponSlots.length){
+            System.out.println("Вы ввели не существующий слот");
+            return;
+        }else{
+            Weapon weapon = weaponSlots[slot];
+            weapon.shot();
+        }
+
+
+
     }
 
 }
